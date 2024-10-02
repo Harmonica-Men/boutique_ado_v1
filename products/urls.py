f69b242474cf6586a5b0444e53ae281b77b380c9
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-02 11:04:52 +0000

    add product_detail

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import ProductDetailView
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<int:product_id>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
 from .models import Product
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
@@ -13,3 +17,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+class ProductDetailView(DetailView):
+    """ A class-based view to show individual product details """
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+    def get_object(self):
+        product_id = self.kwargs.get("product_id")
+        return get_object_or_404(Product, pk=product_id)
+
