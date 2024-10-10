66dec92b27914657aec1d62abc8aaf19d0060687
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-10 11:57:04 +0000

    added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q8KSKFaOdtJcPerRtxXhlce0Rm0laUgmP4Beapse6lZyBcJ8TSLOhW70fJQNsAohs0KJxo1yLsH3UTGuvZikQqN00aXTprB8x',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
