e2d51c7f867dee1a1d10018589afa3ef10b807ed
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-10 19:32:00 +0000

    Set webhook

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,23 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe_secret_key = 'sk_test_51Q8KSKFaOdtJcPergBWHFArKu1pzLjbf8bDFHVHlELTwh59QyXWTw1MyrL3YL6DRbddpOJ1GrLI696N4xNhgj8hc00UanYED31'
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -15,6 +33,7 @@ def checkout(request):
     stripe_public_key = 'pk_test_51Q8KSKFaOdtJcPerRtxXhlce0Rm0laUgmP4Beapse6lZyBcJ8TSLOhW70fJQNsAohs0KJxo1yLsH3UTGuvZikQqN00aXTprB8x'
     stripe_secret_key = 'sk_test_51Q8KSKFaOdtJcPergBWHFArKu1pzLjbf8bDFHVHlELTwh59QyXWTw1MyrL3YL6DRbddpOJ1GrLI696N4xNhgj8hc00UanYED31'
     
+
     if request.method == 'POST':
         bag = request.session.get('bag', {})
 
